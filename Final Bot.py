da2bd9e3e5b08b2826f473f0bf3d4e55581667b8
invisibleflame <bhuvanaggarwal9@gmail.com>
2020-06-20 16:16:12 +0530

    all set

--- Final Bot.py
+++ Final Bot.py
@@ -12,10 +12,113 @@ from newsapi import NewsApiClient
 import requests
 from gtts import gTTS
 from Adafruit_IO import RequestError, client, Feed
-ADAFRUIT_IO_USERNAME='omkarghugarkar7'
-ADAFRUIT_IO_KEY='aio_MSXw66ASir4ko54GJlldZQAat1Tl'
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.response_selection import get_first_response
 
-aio=Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
+cbot = ChatBot('cBot',
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': "chatterbot.logic.BestMatch",
+            'statement_comparison_function': 'chatterbot.comparisons.levenshtein_distance',
+            'response_selection_method': get_first_response
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer')
+trainer = ListTrainer(cbot)
+
+omkar1=["Hi",
+"Hello, how are you ?",
+"I am fine, what about you ?",
+"I am fine too",
+"What you think of life ?",
+"Life is Beautiful",
+"What is your name?",
+"My name is SASHA",
+"WHo created you ?",
+"I was created by team Automated",
+"What can you do",
+"I can turn on your various devices such as fan, light,etc",
+"Can I talk to you for long time",
+"Yes you can talk to me for Long time",
+"What our you?",
+"I am a software cum Hardware combination for you betterment",
+"What makes you different from others",
+"I am more skilful than all others",
+"Bye",
+"Bye, It was great working for you"]
+
+omkar2=["Hey",
+"Hello, how are you ?",
+"I am a little stressed, feeling sad",
+"Just be patient, things will improve",
+"Yes, thanks",
+"Your's Welcome",
+"Bye",
+"Bye, It was great working for you"]
+
+omkar3=["Hello",
+"Hello, how it's going ?",
+"Life is chill",
+"Good to here",
+"Where do you live ?",
+"I live in ROM memory",
+"Where are you from ?",
+"I am from the world of great creativity for making world a Happy Space",
+"Are you a Human?",
+"No, I am not a Human...",
+"Bye",
+"Bye, It was great working for you"]
+
+omkar4=["Hi there",
+"Hello, how it's going?",
+"A bit sad and worried",
+"Just be patient, things will improve",
+"Can I ask you a Question?",
+"What is your Question",
+"In which country do you live",
+"I was made in India"]
+
+fhand1=open('my-data/conversation.yml').readlines()
+fhand2=open('my-data/conversation.yml').readlines()
+fhand3=open('my-data/Artificial_intelligence.yml').readlines()
+fhand4=open('my-data/conversation.yml').readlines()
+fhand5=open('my-data/emotion.yml').readlines()
+fhand6=open('my-data/film.yml').readlines()
+fhand7=open('my-data/food.yml').readlines()
+fhand9=open('my-data/GK.yml').readlines()
+fhand10=open('my-data/IT.yml').readlines()
+fhand11=open('my-data/jokes_fun.yml').readlines()
+fhand12=open('my-data/market_money.yml').readlines()
+fhand13=open('my-data/psychology.yml').readlines()
+fhand14=open('my-data/space_and_science.yml').readlines()
+fhand15=open('my-data/Sport_games.yml').readlines()
+
+trainer.train(fhand1)
+trainer.train(fhand2)
+trainer.train(fhand3)
+trainer.train(fhand4)
+trainer.train(fhand5)
+trainer.train(fhand6)
+trainer.train(fhand7)
+trainer.train(fhand9)
+trainer.train(fhand10)
+trainer.train(fhand11)
+trainer.train(fhand12)
+trainer.train(fhand13)
+trainer.train(fhand14)
+trainer.train(fhand15)
+trainer.train(omkar1)
+trainer.train(omkar2)
+trainer.train(omkar3)
+trainer.train(omkar4)
+
+
+ADAFRUIT_IO_USERNAME='omkarghugarkar7'
+ADAFRUIT_IO_KEY='aio_CtpC53gdcpsAQLVV7fAwEG0qFD9e'
+
+aio=client.Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
 
 newsapi = NewsApiClient(api_key='1cec50aadff94c538a68e0d626bdb1f4')
 
@@ -336,7 +439,7 @@ while (True):
         bot.send_message(chat_id, random_joke)
         continue
 
-    else:
+    elif "on" in recentmessage.lower() or "off" in recentmessage.lower():
         name = get_key(chat_id)
         text = "Hi {}! Your command is loading please wait!".format(name)
         bot.send_message(chat_id, text)
@@ -368,8 +471,8 @@ while (True):
                     # GPIO.output(led, 1)
                     bot.send_message(chat_id, ref)
                     fan = 1
-                    fan-status=aio.feeds('fan-status')
-                    aio.send_data(fan-status.key,'ON')    
+                    fan_status =aio.feeds('fan-status')
+                    aio.send_data(fan_status.key,'ON')
                 continue
 
             if 'light' in recentmessage.lower():
@@ -380,8 +483,8 @@ while (True):
                     # GPIO.output(led, 1)
                     bot.send_message(chat_id, ref)
                     lights = 1
-                    welcome-feed=aio.feeds('welcome-feed')
-                    aio.send_data(welcome-feed.key,'ON')
+                    welcome_feed=aio.feeds('welcome-feed')
+                    aio.send_data(welcome_feed.key,'ON')
                 continue
             else:
                 bot.send_message(chat_id, "Invalid command, please try again and specify the appliance properly!")
@@ -409,8 +512,8 @@ while (True):
                     # GPIO.output(led, 1)
                     bot.send_message(chat_id, ref)
                     fan = 0
-                    fan-status=aio.feeds('fan-status')
-                    aio.send_data(fan-status.key,'ON') 
+                    fan_status=aio.feeds('fan-status')
+                    aio.send_data(fan_status.key,'OFF')
                 continue
 
             if 'light' or 'lights' in recentmessage.lower():
@@ -421,13 +524,19 @@ while (True):
                     # GPIO.output(led, 1)
                     bot.send_message(chat_id, ref)
                     lights = 0
-                    welcome-feed=aio.feeds('welcome-feed')
-                    aio.send_data(welcome-feed.key,'ON')
+                    welcome_feed=aio.feeds('welcome-feed')
+                    aio.send_data(welcome_feed.key,'OFF')
                 continue
             else:
                 bot.send_message(chat_id, "Invalid command, please try again and specify the appliance properly!")
 
-        else:
-            bot.send_message(chat_id, "INVALID COMMAND! TRY AGAIN")
 
-        time.sleep(4)
+    else:
+
+        user_input = recentmessage
+
+        bot_response = str(cbot.get_response(user_input))
+
+        bot.send_message(chat_id, bot_response)
+
+
